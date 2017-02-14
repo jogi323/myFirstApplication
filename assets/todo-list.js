a97d1995bb2d7534adca712da93ce431a731bc1a
Joginaidu Gopisetti <jgopisetti@corp.innominds.com>
2017-02-15 00:38:50 +0530

    todoCommit

--- assets/todo-list.js
+++ assets/todo-list.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('form').on('submit',function(){
     var item=$('form input');
     var todo={ item: item.val()};
-    console.log("dfsef");
+  
     $.ajax({
       type : 'post',
       url :  '/todo',
@@ -15,7 +15,7 @@ $(document).ready(function(){
     return false;
   });
 $('li').on('click',function(){
-  var item =$(this).text().replace(/ /g,"");
+  var item =$(this).text().trim();
   $.ajax({
     type : 'DELETE',
     url :  '/todo/' + item,
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,27 +1,57 @@
 var bodyParser =require('body-parser');
 var mongoose =require('mongoose');
+mongoose.connect('mongodb://localhost/my_items');
 var data=  [{item:'Play Cricket'},{item:'Watch Cricket'},{item:'Listen to Music'}];
-
+var Comment = new mongoose.Schema({
+  item: { type: String}
+});
+var MyModel = mongoose.model('items', Comment);
+// var itemOne=MyModel({item:'Good Evening'}).save(function(err){
+//   if(err) throw err;
+//   console.log("item saved.");
+// });
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app){
   app.get('/',function(req,res){
-    res.render('todo',{todos:data});
+    MyModel.find({},function(err,data){
+      if(err) {
+        console.log(err);
+      }else{
+          res.render('todo',{todos:data});
+      }
+    });
+
   });
 app.get('/todo',function(req,res){
-  res.render('todo',{todos:data});
+  MyModel.find({},function(err,data){
+    if(err) {
+      console.log(err);
+    }else{
+        res.render('todo',{todos:data});
+    }
+  });
+
 });
 app.post('/todo',urlencodedParser,function(req,res){
-  data.push(req.body);
-  res.json(data);
+  console.log(req.body);
+  var newItems=MyModel(req.body).save(function(err,data){
+    if(err) {
+      console.log(err);
+    }else{
+        res.json(data);
+    }
+  });
+
 });
 app.delete('/todo/:item',function(req,res){
-  for (var i=0;i<data.length;i++){
-       if(data[i].item.replace(/ /g, "") == req.params.item.replace(/ /g, "")){
-           data.splice(i,1);
-       };
-
-    };
-  res.json(data);
+  console.log(req.params.item);
+  MyModel.remove({item:req.params.item},function(err,data){
+    if(err) {
+      console.log(err);
+    }else{
+        res.json(data);
+    }
+  })
 });
 };
